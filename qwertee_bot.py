788598f40a322642da5deb4bcdc43a91d96bb057
jiwidi <fhjaime96@gmail.com>
2017-05-09 09:01:03 +0200

    New funcionability, now it sends the title too

--- qwertee_bot.py
+++ qwertee_bot.py
@@ -3,27 +3,31 @@ import shutil
 import telebot
 import time
 
-def getURLfromString(text):
-    return text.rsplit(".jpg")[0].rsplit('source srcset="')[1].replace("mens","zoom")
+def getDATAfromString(text):
+    indT=text.find('data-name="')
+    title=text[indT:indT+250].rsplit(' data-id="')[0].rsplit('data-name="')[1].replace('"',"")
+    return [text.rsplit(".jpg")[0].rsplit('source srcset="')[1].replace("mens","zoom"),title]
+   
 def getImages():
     r = requests.get('https://www.qwertee.com/')
     result = []
     ind=0
     for i in range(3):
         ind=r.text.find('<picture>',ind+1)
-        url=getURLfromString(r.text[ind:ind+300])
-        z = requests.get("https:"+url+".jpg", stream=True)
+        url=getDATAfromString(r.text[ind:ind+1000])
+        z = requests.get("https:"+url[0]+".jpg", stream=True)
         if z.status_code == 200:
-            with open("daily"+str(i)+".jpg", 'wb') as f:
+            with open(url[1]+".jpg", 'wb') as f:
                 z.raw.decode_content = True
                 shutil.copyfileobj(z.raw, f)
-        #result.append(url)
-        #print(url)
+        result.append(url[1])
+        
+    return result
 
-getImages()
-bot=telebot.TeleBot('open('token.txt').read())
+names=getImages()
+bot=telebot.TeleBot(open('token.txt').read().strip())
 for u in range(3):
-    file = open("daily"+str(u)+".jpg", 'rb')
-    bot.send_photo("@DailyQwertee", file)
-    #bot.send_photo(5901753, file) just for testing
+    file = open(names[u]+".jpg", 'rb')
+    bot.send_photo("@DailyQwertee", file,names[u])
+    #bot.send_photo(5901753, file,names[u]) #just for testing
 
